a858674568d75d56ed21307795a551fb74678177
HabibXgg <habib123.h1h@gmail.com>
2023-11-30 15:19:48 +0700

    aowkoawkoa

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,32 +4,53 @@ import { useNavigate } from "react-router-dom";
 import "./Home.css";
 
 export default function Home() {
+  const [plants, setPlants] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [filteredPlants, setFilteredPlants] = useState([]);
 
-  const [plant, setPlant] = useState([]);
   useEffect(() => {
-    fetch("https://api.jsonbin.io/v3/b/6561395c54105e766fd4d490?meta=false")
-      .then((response) => response.json())
-      .then((json) => setPlant(json))
-  }, []);
+    const fetchPlants = async () => {
+      const response = await fetch("https://api.jsonbin.io/v3/b/6561395c54105e766fd4d490?meta=false");
+      const json = await response.json();
+      setPlants(json);
+      setFilteredPlants(json);
+    };
+
+    fetchPlants();
+  }, []); // Added empty dependency array
+
+  const handleSearch = (searchTerm) => {
+    setSearchTerm(searchTerm);
+    const filtered = plants.filter((plant) =>
+      plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+    );
+    setFilteredPlants(filtered);
+  };
 
   let navigate = useNavigate();
 
   return (
     <>
+      <div id="search-bar">
+        <input
+          type="text"
+          placeholder="Search plants..."
+          value={searchTerm}
+          onChange={(e) => handleSearch(e.target.value)}
+        />
+      </div>
       <p id="home">Tanaman Hias</p>
-      {plant.map((item, index) => (
+      {filteredPlants.map((item, index) => (
         <Fragment key={item.id}>
           <CardSmall
             title={item.name}
             img={item.image}
             desc={item.species}
-            //onClick={() => window.location.replace(`http://localhost:3000/home/${item.title}`)}
-            onClick={() =>  navigate(`detail`, { state: item })}
+            onClick={() => navigate(`detail`, { state: item })}
           />
-          {plant.length === index + 1 && <div style={{ marginBottom: 80 }} />}
+          {filteredPlants.length === index + 1 && <div style={{ marginBottom: 80 }} />}
         </Fragment>
       ))}
     </>
   );
 }
-
